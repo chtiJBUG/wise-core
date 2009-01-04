a593d75d3785cb1f4efd108ccb13027a80136426
jervisliu <jervisliu@a17ab928-1b57-0410-8599-cedcd8a065c6>
2009-01-04 04:55:54 +0000

    Trivial update

--- src/org/jboss/wise/core/client/jaxrs/impl/RSDynamicClientImpl.java
+++ src/org/jboss/wise/core/client/jaxrs/impl/RSDynamicClientImpl.java
@@ -3,17 +3,17 @@
  * individual contributors as indicated by the @authors tag. See the
  * copyright.txt in the distribution for a full listing of individual
  * contributors.
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -49,7 +49,7 @@ import org.jboss.wise.core.mapper.WiseMapper;
 
 
 /*
- * TODO: 
+ * TODO:
  * 1. return headers
  * 2. Support MultipartRequestEntity
  * 3. Support HttpClient properties
@@ -70,45 +70,45 @@ public class RSDynamicClientImpl implements RSDynamicClient {
     private HttpMethod httpMethod;
     private HttpClient httpClient;
     private Map<String, String> requestHeaders = new HashMap<String, String>();
-    
-    
+
+
     /**
      * Invoke JAXRS service.
-     * 
-     * @param resourceURI 
+     *
+     * @param resourceURI
      * @param produceMediaTypes: default to "* / *"
      * @param consumeMediaTypes: default to "* / *"
-     * @param httpMethod 
-     */   
+     * @param httpMethod
+     */
     public RSDynamicClientImpl(String resourceURI, String produceMediaTypes, String consumeMediaTypes, HttpMethod httpMethod) {
         this.resourceURI = resourceURI;
         this.produceMediaTypes = produceMediaTypes;
         this.consumeMediaTypes = consumeMediaTypes;
-        this.httpMethod = httpMethod; 
-        
-        this.httpClient = new HttpClient();    
+        this.httpMethod = httpMethod;
+
+        this.httpClient = new HttpClient();
     }
-    
+
     public RSDynamicClientImpl(String resourceURI, String produceMediaTypes, String consumeMediaTypes, HttpMethod httpMethod, Map<String, String> requestHeaders) {
         this.resourceURI = resourceURI;
         this.produceMediaTypes = produceMediaTypes;
         this.consumeMediaTypes = consumeMediaTypes;
-        this.httpMethod = httpMethod; 
+        this.httpMethod = httpMethod;
         this.requestHeaders = requestHeaders;
-        
-        this.httpClient = new HttpClient();    
+
+        this.httpClient = new HttpClient();
     }
-    
+
     public String getResourceURI(){
-        return resourceURI;    
+        return resourceURI;
     }
 
     public String getUser(){
-        return user;    
+        return user;
     }
 
     public String getPassword(){
-        return password;  
+        return password;
     }
 
     public HttpMethod getHttpMethod() {
@@ -118,11 +118,11 @@ public class RSDynamicClientImpl implements RSDynamicClient {
     public String getProduceMediaTypes() {
         return produceMediaTypes;
     }
-    
+
     public String getConsumeMediaTypes() {
         return consumeMediaTypes;
     }
-    
+
     public InvocationResult invoke(Map<String, Object> inputObjects,
                                     WiseMapper mapper) {
         //NOT SUPPORTED
@@ -149,27 +149,21 @@ public class RSDynamicClientImpl implements RSDynamicClient {
                                     WiseMapper mapper) {
         return invoke(new ByteArrayRequestEntity(request), mapper);
     }
-    
+
     public InvocationResult invoke(File request,
                                     WiseMapper mapper ) {
         return invoke(new FileRequestEntity(request, produceMediaTypes), mapper);
     }
-    
+
     public InvocationResult invoke() {
         RequestEntity requestEntity = null;
         return invoke(requestEntity, null);
     }
 
-    //TODO: 1. return statusCode
-    //      2. Support MultipartRequestEntity
-    //      3. Support HttpClient properties
-    //      4. Support setting headers
-    //      5. Return the result in formats other than InputStream
-     
-    public InvocationResult invoke(RequestEntity requestEntity, WiseMapper mapper) {        
+    public InvocationResult invoke(RequestEntity requestEntity, WiseMapper mapper) {
         InvocationResult result = null;
         Map<String, Object> responseHolder = new HashMap<String, Object>();
-        
+
         if (HttpMethod.GET == httpMethod) {
             GetMethod get = new GetMethod(resourceURI);
             setRequestHeaders(get);
@@ -179,89 +173,89 @@ public class RSDynamicClientImpl implements RSDynamicClient {
                 //TODO: Use InputStream
                 String response = get.getResponseBodyAsString();
                 responseHolder.put(InvocationResult.STATUS, new Integer(statusCode));
-                
+
                 result = new InvocationResultImpl(InvocationResult.RESPONSE, response, responseHolder);
-                
+
                 // System.out.print(response);
             } catch(IOException e) {
-                //TODO:               
+                //TODO:
             } finally {
                 get.releaseConnection();
             }
         } else if (HttpMethod.POST == httpMethod) {
             PostMethod post = new PostMethod(resourceURI);
             setRequestHeaders(post);
-          
+
             post.setRequestEntity(requestEntity);
- 
+
             try {
                 int statusCode = httpClient.executeMethod(post);
                 String response = post.getResponseBodyAsString();
                 responseHolder.put(InvocationResult.STATUS, new Integer(statusCode));
-                
+
                 result = new InvocationResultImpl(InvocationResult.RESPONSE, response, responseHolder);
-                
+
                 // System.out.print(response);
             } catch(IOException e) {
-                //TODO:              
+                //TODO:
             } finally {
                 post.releaseConnection();
             }
         } else if (HttpMethod.PUT == httpMethod) {
             PutMethod put = new PutMethod(resourceURI);
             setRequestHeaders(put);
-           
+
             put.setRequestEntity(requestEntity);
- 
+
             try {
                 int statusCode = httpClient.executeMethod(put);
                 String response = put.getResponseBodyAsString();
                 responseHolder.put(InvocationResult.STATUS, new Integer(statusCode));
-                
+
                 result = new InvocationResultImpl(InvocationResult.RESPONSE, response, responseHolder);
-                
+
                 // System.out.print(response);
             } catch(IOException e) {
-                //TODO:             
+                //TODO:
             } finally {
                 put.releaseConnection();
             }
         } else if (HttpMethod.DELETE == httpMethod) {
             DeleteMethod delete = new DeleteMethod(resourceURI);
             setRequestHeaders(delete);
-          
+
             try {
                 int statusCode = httpClient.executeMethod(delete);
                 String response = delete.getResponseBodyAsString();
                 responseHolder.put(InvocationResult.STATUS, new Integer(statusCode));
-                
+
                 result = new InvocationResultImpl(InvocationResult.RESPONSE, response, responseHolder);
-                
+
                 // System.out.print(response);
             } catch(IOException e) {
-                //TODO:              
+                //TODO:
             } finally {
                 delete.releaseConnection();
             }
         }
-        
-        return result;        
+
+        return result;
     }
-   
-    private void setRequestHeaders(HttpMethodBase method) {        
+
+    private void setRequestHeaders(HttpMethodBase method) {
         if (produceMediaTypes != null) {
             method.setRequestHeader("Content-Type", produceMediaTypes);
         }
-        
+
         if (consumeMediaTypes != null) {
             method.setRequestHeader("Accept", consumeMediaTypes);
         }
-        
+
         for (String headerName : requestHeaders.keySet()) {
             String headerValue = requestHeaders.get(headerName);
 
             method.setRequestHeader(headerName, headerValue);
         }
     }
-    
+
 }
