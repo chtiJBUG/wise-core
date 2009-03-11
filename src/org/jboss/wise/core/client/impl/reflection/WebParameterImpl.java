1215f5f0f659c49f6a5caae818fe2dbd9ce9fd58
maeste <maeste@a17ab928-1b57-0410-8599-cedcd8a065c6>
2009-03-11 20:44:18 +0000

    Using apache HashCodeBuilder and EqualsBuilder

--- src/org/jboss/wise/core/client/impl/reflection/WebParameterImpl.java
+++ src/org/jboss/wise/core/client/impl/reflection/WebParameterImpl.java
@@ -25,8 +25,10 @@ package org.jboss.wise.core.client.impl.reflection;
 import java.lang.reflect.Type;
 import javax.jws.WebParam;
 import javax.jws.WebParam.Mode;
-import org.jboss.wise.core.client.WebParameter;
 import net.jcip.annotations.Immutable;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.jboss.wise.core.client.WebParameter;
 
 /**
  * Holds single parameter's data required for an invocation
@@ -104,13 +106,7 @@ public class WebParameterImpl implements WebParameter {
      */
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((mode == null) ? 0 : mode.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + position;
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 
     /**
@@ -120,21 +116,7 @@ public class WebParameterImpl implements WebParameter {
      */
     @Override
     public boolean equals( Object obj ) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (!(obj instanceof WebParameterImpl)) return false;
-        final WebParameterImpl other = (WebParameterImpl)obj;
-        if (mode == null) {
-            if (other.mode != null) return false;
-        } else if (!mode.equals(other.mode)) return false;
-        if (name == null) {
-            if (other.name != null) return false;
-        } else if (!name.equals(other.name)) return false;
-        if (position != other.position) return false;
-        if (type == null) {
-            if (other.type != null) return false;
-        } else if (!type.equals(other.type)) return false;
-        return true;
+        return EqualsBuilder.reflectionEquals(this, obj);
     }
 
 }
