8a4e069b1e69b8ce7870256982b46908e54263d0
maeste <maeste@a17ab928-1b57-0410-8599-cedcd8a065c6>
2009-04-21 15:47:36 +0000

    WISE-141, WISE-144

--- core/src/main/java/org/jboss/wise/core/mapper/SmooksMapper.java
+++ core/src/main/java/org/jboss/wise/core/mapper/SmooksMapper.java
@@ -23,19 +23,30 @@
 package org.jboss.wise.core.mapper;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+
 import javax.xml.transform.Source;
+
 import net.jcip.annotations.Immutable;
 import net.jcip.annotations.ThreadSafe;
+
 import org.apache.log4j.Logger;
 import org.jboss.wise.core.client.WSDynamicClient;
 import org.jboss.wise.core.exception.MappingException;
 import org.jboss.wise.core.exception.WiseRuntimeException;
 import org.milyn.Smooks;
+import org.milyn.SmooksUtil;
+import org.milyn.cdr.SmooksResourceConfiguration;
+import org.milyn.cdr.SmooksResourceConfigurationList;
+import org.milyn.cdr.XMLConfigDigester;
 import org.milyn.container.ExecutionContext;
 import org.milyn.container.plugin.PayloadProcessor;
 import org.milyn.event.report.HtmlReportGenerator;
 import org.milyn.payload.JavaResult;
+import org.milyn.profile.DefaultProfileSet;
+import org.milyn.profile.ProfileStore;
+import org.milyn.profile.UnknownProfileMemberException;
 import org.milyn.resource.URIResourceLocator;
 
 /**
@@ -50,41 +61,60 @@ public class SmooksMapper implements WiseMapper {
     private final Logger log = Logger.getLogger(SmooksMapper.class);
 
     private final Smooks smooks;
+
     private final String smooksReport;
 
-    public SmooksMapper( String smooksResource,
-                         WSDynamicClient client ) {
-        this(smooksResource, null, client);
+    public SmooksMapper(String smooksResource, WSDynamicClient client) {
+	this(smooksResource, null, client);
     }
 
-    public SmooksMapper( String smooksResource,
-                         String smooksReport,
-                         WSDynamicClient client ) {
-        try {
-            smooks = client.getSmooksInstance();
-            smooks.addConfigurations("smooks-resource", new URIResourceLocator().getResource(smooksResource));
-            this.smooksReport = smooksReport;
-        } catch (Exception e) {
-            throw new WiseRuntimeException("failde to create SmooksMapper", e);
-        }
+    public SmooksMapper(String smooksResource, String smooksReport, WSDynamicClient client) {
+	try {
+	    smooks = client.getSmooksInstance();
+	    try {
+		ProfileStore profileStore = smooks.getApplicationContext().getProfileStore();
+		profileStore.getProfileSet(Integer.toString(this.hashCode()));
+	    } catch (UnknownProfileMemberException e) {
+
+		synchronized (SmooksMapper.class) {
+		    // Register the message flow within the Smooks context....
+		    SmooksUtil
+			    .registerProfileSet(DefaultProfileSet.create(Integer.toString(this.hashCode()), new String[] {}), smooks);
+		}
+	    }
+	    SmooksResourceConfigurationList list = XMLConfigDigester.digestConfig(new URIResourceLocator()
+		    .getResource(smooksResource), "wise");
+	    for (int i = 0; i < list.size(); i++) {
+		SmooksResourceConfiguration smookResourceElement = list.get(i);
+		smookResourceElement.setTargetProfile(Integer.toString(this.hashCode()));
+		SmooksUtil.registerResource(smookResourceElement, smooks);
+	    }
+	    // smooks.addConfigurations(Integer.toString(this.hashCode()), new
+	    // URIResourceLocator().getResource(smooksResource));
+
+	    this.smooksReport = smooksReport;
+	} catch (Exception e) {
+	    throw new WiseRuntimeException("failde to create SmooksMapper", e);
+	}
     }
 
     // package protected for test purpose
-    /*package*/ExecutionContext initExecutionContext( String smooksReport ) {
-        ExecutionContext executionContext = smooks.createExecutionContext();
-        if (smooksReport != null) {
-            try {
-                executionContext.setEventListener(new HtmlReportGenerator(smooksReport));
-            } catch (IOException e) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Error during loading/instanciating Html report generator (" + smooksReport
-                              + ") with exception message: " + e.getMessage());
-                    log.info("Wise will continue without it");
+    /* package */ExecutionContext initExecutionContext(String smooksReport) {
+	ExecutionContext executionContext = smooks.createExecutionContext(Integer.toString(this.hashCode()));
+	if (smooksReport != null) {
+	    try {
+		executionContext.setEventListener(new HtmlReportGenerator(smooksReport));
+	    } catch (IOException e) {
+		if (log.isDebugEnabled()) {
+		    log
+			    .debug("Error during loading/instanciating Html report generator (" + smooksReport + ") with exception message: " + e
+				    .getMessage());
+		    log.info("Wise will continue without it");
 
-                }
-            }
-        }
-        return executionContext;
+		}
+	    }
+	}
+	return executionContext;
 
     }
 
@@ -92,25 +122,27 @@ public class SmooksMapper implements WiseMapper {
      * apply this mapping to original object
      * 
      * @param originalObjects
-     * @return Map returned is typically used to invoke webservice operations. To do this, beanids defined in smooks config (and
-     *         used here as Map's keys) have to be the parameters names as defined in wsdl/wsconsume generated classes
+     * @return Map returned is typically used to invoke webservice operations.
+     *         To do this, beanids defined in smooks config (and used here as
+     *         Map's keys) have to be the parameters names as defined in
+     *         wsdl/wsconsume generated classes
      * @throws MappingException
      */
-    public Map<String, Object> applyMapping( Object originalObjects ) throws MappingException {
-        ExecutionContext executionContext = initExecutionContext(smooksReport);
-        Source source;
-        JavaResult result = new JavaResult();
-        // Configure the execution context to generate a report...
-        org.milyn.container.plugin.PayloadProcessor payloadProcessor = new PayloadProcessor(
-                                                                                            smooks,
-                                                                                            org.milyn.container.plugin.ResultType.JAVA);
-        Map<String, Object> returnMap = (Map<String, Object>)payloadProcessor.process(originalObjects, executionContext);
-        // workaround when we should use smooks to extract a single value
-        // Have a look to SmooksMapperTest.shouldMapToSingleInput() for an example of use
-        if (returnMap.size() == 1 && returnMap.get("singleInput") != null) {
-            returnMap = (Map<String, Object>)returnMap.get("singleInput");
-        }
-        return returnMap;
+    public Map<String, Object> applyMapping(Object originalObjects) throws MappingException {
+	ExecutionContext executionContext = initExecutionContext(smooksReport);
+	Source source;
+	JavaResult result = new JavaResult();
+	// Configure the execution context to generate a report...
+	org.milyn.container.plugin.PayloadProcessor payloadProcessor = new PayloadProcessor(smooks,
+		org.milyn.container.plugin.ResultType.JAVA);
+	Map<String, Object> returnMap = (Map<String, Object>) payloadProcessor.process(originalObjects, executionContext);
+	// workaround when we should use smooks to extract a single value
+	// Have a look to SmooksMapperTest.shouldMapToSingleInput() for an
+	// example of use
+	if (returnMap.size() == 1 && returnMap.get("singleInput") != null) {
+	    returnMap = (Map<String, Object>) returnMap.get("singleInput");
+	}
+	return returnMap;
 
     }
 
